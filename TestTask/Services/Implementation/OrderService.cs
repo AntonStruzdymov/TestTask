c6a5f4adbc9084bf44e55f335e31774b853e58a6
Anton Struzdymov <struzdymov@gmail.com>
2023-10-02 21:17:18 +0300

    Add project files.

---
+++ TestTask/Services/Implementation/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementation
+{
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<Order> GetOrder()
+        {
+            var order = _context.Orders.OrderByDescending(a=>a.Quantity*a.Price).FirstOrDefault();
+            return Task.FromResult(order);
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            var orders = _context.Orders.Where(a=>a.Quantity>10).ToList();
+            return Task.FromResult(orders);
+        }
+    }
+}
---
+++ TestTask/Services/Implementation/UserService.cs
@@ -0,0 +1,28 @@
+﻿using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementation
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<User> GetUser()
+        {
+            var user = _context.Users.OrderByDescending(a=>a.Orders.Count).FirstOrDefault();
+            return Task.FromResult(user);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            var users = _context.Users.Where(a=>a.Status.Equals(UserStatus.Inactive)).ToList();
+            return Task.FromResult(users);
+        }
+    }
+}
